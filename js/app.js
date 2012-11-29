579edd97e795751b318cf368d40a45c21c37f587
Tony Webster <tonywebster@gmail.com>
2012-11-28 22:48:58 -0600

    Header layout

--- js/app.js
+++ js/app.js
@@ -5,6 +5,10 @@
 		$('body').css('font-size', '200%');
 	});
 
+	$('#js-high-contrast').click(function() {
+		$('body').addClass('high-contrast');
+	});
+
      $(window)
         .sausage({
             content: function (i, $page) {
